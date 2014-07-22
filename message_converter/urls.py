4459c5a1d9696604f60c2ab7a5a060439f9cffcc
Matthew Ghantous <matthew@againfaster.com>
2014-07-22 14:46:54 -0400

    Put action parameter in webhook URL because Wombat can't handle two webhooks with same URL.

--- message_converter/urls.py
+++ message_converter/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, include, url
 from message_converter.views import ApiProjectView
 
 urlpatterns = patterns('',
-                       url(r'^api/(?P<project_name>[\w-]+)[/]?$', ApiProjectView.as_view(), name='api_project_view'),)
+                       url(r'^api/(?P<project_name>[\w-]+)[/]?$', ApiProjectView.as_view(), name='api_project_view'),
+                       url(r'^api/(?P<project_name>[\w-]+)/(?P<action>[\w-]+)[/]?$', ApiProjectView.as_view(), name='api_project_view'),)
 
